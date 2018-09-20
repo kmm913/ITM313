8316bf34d5a6073e56aa6e179611f617e4255c61
kmm913 <kyle.m.mahoney@gmail.com>
2018-09-20 17:41:05 -0500

    timer test code

---
+++ Countdown.py
@@ -0,0 +1,15 @@
+import time
+
+timeLimit = 10
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
+    print('Time is up!')
+
+#call the countdown
+countdown(timeLimit)    
